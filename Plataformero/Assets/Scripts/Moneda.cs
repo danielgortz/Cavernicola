bda7abc79b1bdad375e47fb7da1c4affe825834c
danielgortz <142053799+danielgortz@users.noreply.github.com>
2023-11-23 21:30:15 -0600

    pociones

--- Plataformero/Assets/Scripts/Moneda.cs
+++ Plataformero/Assets/Scripts/Moneda.cs
@@ -4,36 +4,34 @@ using UnityEngine;
 
 public class Moneda : MonoBehaviour
 {
-    public int valorMoneda = 10;
-    private Animator  miAnimador;
+   
+
+    private Animator miAnimador;
+    public float monedas = 0;
+    public Personaje heroe;
     // Start is called before the first frame update
     void Start()
     {
-        miAnimador = GetComponent<Animator>();  
+        miAnimador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D agarrar)
     {
-        print(name + "hizo colision con" + collision.gameObject.name);
-        GameObject otro = collision.gameObject;
+        Moneda moneda = GetComponent<Moneda>();
+        print(name + "hizo colision con" + agarrar.gameObject.name);
+        GameObject otro = agarrar.gameObject;
         if (otro.tag == "Player")
         {
-            //Accede al componente de tipo Personaje del objeto con el que choquè
-            Personaje elPerso = otro.GetComponent<Personaje>();
-            //Aplico el daño al otro invocando el metodo hacer daño
-            elPerso.score = elPerso.score + valorMoneda;
-            miAnimador.SetTrigger("desaparecer");
+            miAnimador.SetTrigger("OBTENER");
+            heroe.score++;
+            monedas++;
             GetComponent<Collider2D>().enabled = false;
-            Destroy(gameObject,1.5f);
+            Destroy(gameObject, 2);
         }
     }
-
 }
---
+++ Plataformero/Assets/Scripts/PocionCurar.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PocionCurar : MonoBehaviour
+{
+    private Animator miAnimador;
+    public int curarPuntos = 20;
+    public Personaje heroe;
+    void Start()
+    {
+        miAnimador = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D curar)
+    {
+        PocionCurar pocion = GetComponent<PocionCurar>();
+        print(name + "hizo colision con" + curar.gameObject.name);
+        GameObject otro = curar.gameObject;
+        if (otro.tag == "Player" && heroe.hp < 80)
+        {
+            miAnimador.SetTrigger("OBTENER");
+            heroe.hp = heroe.hp + curarPuntos;
+            GetComponent<Collider2D>().enabled = false;
+            Destroy(gameObject, 3);
+        }
+    }
+}
